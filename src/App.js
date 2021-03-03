56ca50dc155b5d693074fa931f83982bd2cdaf93
ska7 <nikkofann@gmail.com>
2021-03-03 16:47:32 +0200

    heroku

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { EventInfoModalLayout } from "./components/EventInfoModalLayout";
 import { FavoritesHeartButton } from "./components/FavoritesHeartButton";
 import { Input } from "./components/Input";
 import { BandEventsWrapper } from "./components/BandEventsWrapper";
+import { Test } from "./components/Test";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <FavoritesHeartButton />
         <Input />
         <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
           <Route path="/band/:bandID" component={BandEventsWrapper} />
           <Route path="/event/:eventID" component={EventInfoModalLayout} />
         </Switch>
