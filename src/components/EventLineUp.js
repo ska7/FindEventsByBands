b31920365d6ca01df4dec45b0a0c66c2a34ecd22
ska7 <nikkofann@gmail.com>
2021-04-18 23:09:52 +0300

    changes

--- src/components/EventLineUp.js
+++ src/components/EventLineUp.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 
-import { Container, List, ListItem, Typography } from "@material-ui/core";
+import {
+  Container,
+  List,
+  ListItem,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { IconButton } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import TableRow from "@material-ui/core/TableRow";
 import { Collapse } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -20,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
   },
   artist: {
     fontSize: "13px",
+    color: "white",
   },
   icon: {
     width: "50px",
@@ -34,9 +46,27 @@ const useStyles = makeStyles((theme) => ({
   lineUp: {
     width: "100%",
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
   },
 }));
+
+const createTableRow = (artists, classes) => {
+  // let numberOfRows = (artists.length / 5);
+  const chunks = [];
+
+  for (let i = 5; i < artists.length; i += 5) {
+    const chunk = artists.slice(i, i + 5);
+    chunks.push(
+      <TableRow>
+        {chunk.map((artist) => (
+          <TableCell className={classes.artist}>{artist.displayName}</TableCell>
+        ))}
+      </TableRow>
+    );
+  }
+
+  return chunks;
+};
 
 export const EventLineUp = ({ artists }) => {
   const classes = useStyles();
@@ -57,11 +87,10 @@ export const EventLineUp = ({ artists }) => {
       </Container>
       <Collapse in={isUnfolded} timeout="auto">
         <Typography className={classes.lineupTitle}>LINEUP</Typography>
-        <List className={classes.lineUp}>
-          {artists.map((artist) => (
-            <ListItem className={classes.artist}>{artist.displayName}</ListItem>
-          ))}
-        </List>
+        <TableContainer>
+          <Table className={classes.table} size="small"></Table>
+          <TableBody>{createTableRow(artists, classes)}</TableBody>
+        </TableContainer>
       </Collapse>
     </Container>
   );
